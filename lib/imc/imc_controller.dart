7b699829ead4d75b92ba98b7ed5eb1fc8be93a22
dariopmaciel <dariodepaulamaciel@hotmail.com>
2023-11-29 09:19:20 -0300

    tratamento de error

--- lib/imc/imc_controller.dart
+++ lib/imc/imc_controller.dart
@@ -8,15 +8,30 @@ class ImcController = ImcControllerBase with _$ImcController;
 abstract class ImcControllerBase with Store {
   @observable
   double imc = 0.0;
+  @observable
+  String? error;
+
+  @computed
+  bool get hasError => error != null;
 
   @action
   Future<void> calcularImc({
     required double peso,
     required double altura,
   }) async {
-    imc = 0;
-    // imc = peso / (altura * altura);
-    await Future.delayed(const Duration(seconds: 1));
-    imc = peso / pow(altura, 2);
+    try {
+      imc = 0;
+      error = null;
+      await Future.delayed(const Duration(seconds: 1));
+      //ou
+      // imc = peso / (altura * altura);
+      imc = peso / pow(altura, 2);
+
+      if (imc > 30) {
+        throw Exception();
+      }
+    } catch (e) {
+      error = "Erro ao calcular o IMC";
+    }
   }
 }
