3f5f25a6687d48d371d21fb6e11778b67c336d37
dariopmaciel <dariodepaulamaciel@hotmail.com>
2023-11-30 09:06:45 -0300

    criação de modelo para o observable list page

--- lib/observables/list/observable_list_page.dart
+++ lib/observables/list/observable_list_page.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 
 class ObservableListPage extends StatelessWidget {
+  const ObservableListPage({super.key});
 
-  const ObservableListPage({ super.key });
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text(''),),
-           body: Container(),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Observable List Page'),
+      ),
+      body: Column(children: [
+        ListView(),
+        Row(
+          children: [
+            TextButton(onPressed: () {}, child: const Text("Adicionar")),
+            TextButton(onPressed: () {}, child: const Text("Remover")),
+            TextButton(onPressed: () {}, child: const Text("Carregar")),
+          ],
+        ),
+      ]),
+    );
   }
 }
